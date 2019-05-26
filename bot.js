7c75f4b4ec3176f2a3847f7b6a9e03ab183fdd5a
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-05-26 20:06:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("+play",{type: 'WATCHING'})
+   client.user.setActivity("1play",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
